118396544f8c3ea7d4f788162d563bc84423ed54
yuchi1128 <yukiya20021128@icloud.com>
2024-10-06 00:41:31 +0900

    コンポーネント分割

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
       <header>
-        <h3 className="home__title">請求書送信BOT</h3>
+        <div className="home">
+          <h3 className="home__title">請求書送信BOT</h3>
+        </div>
       </header>
     );
 }
