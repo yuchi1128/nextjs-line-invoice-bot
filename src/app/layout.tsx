431098766133e1be9c0ece5e0c2361ba84babeea
yuchi1128 <yukiya20021128@icloud.com>
2024-10-06 01:05:35 +0900

    コンポーネント分割

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-// import './globals.css';
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body>{children}</body>
-//     </html>
-//   );
-// }
-
-
-
-
-
 import './globals.css';
-import Header from './components/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
-      <body>
-        <Header />
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
+
+
+
+
+
+// import './globals.css';
+// import Header from './components/Header';
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="ja">
+//       <body>
+//         <Header />
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
