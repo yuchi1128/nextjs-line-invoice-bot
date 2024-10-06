54467b3c15baaf40305afabd3adbd96ca942607a
yuchi1128 <yukiya20021128@icloud.com>
2024-10-06 11:45:26 +0900

    コンポーネント分割

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,65 +1,4 @@
-// import { useEffect, useState } from 'react';
-// import liff from '@line/liff';
-
-// interface Profile {
-//   name: string;
-//   picture: string;
-// }
-
-// export default function Header() {
-//   const [profile, setProfile] = useState<Profile>({ name: '', picture: '' });
-
-//   useEffect(() => {
-//     const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
-
-//     if (!liffId) {
-//       console.error('LIFF ID is missing');
-//       return;
-//     }
-
-//     liff
-//       .init({ liffId })
-//       .then(() => {
-//         if (liff.isLoggedIn()) {
-//           fetchUserProfile();
-//         } else {
-//           liff.login();
-//         }
-//       })
-//       .catch((error) => {
-//         console.log('LIFF initialization failed', error);
-//       });
-//   }, []);
-
-//   const fetchUserProfile = async () => {
-//     try {
-//       const idToken = liff.getDecodedIDToken();
-//       setProfile({ name: idToken?.name ?? '', picture: idToken?.picture ?? '' });
-//     } catch (error) {
-//       console.error('Failed to fetch user profile:', error);
-//     }
-//   };
-
-//   return (
-//     <header className="header">
-//       <div className="profile-container">
-//         <img className="profile-image" src={profile.picture} alt="プロフィール" />
-//         <span className="profile-name">{profile.name}さん</span>
-//       </div>
-//     </header>
-//   );
-// }
-
-
-
-
-
-
-
-//Headerでliff初期化をしないようにした　layoutでHeaderを呼び出し
-"use client";
-
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import liff from '@line/liff';
 
 interface Profile {
@@ -71,9 +10,25 @@ export default function Header() {
   const [profile, setProfile] = useState<Profile>({ name: '', picture: '' });
 
   useEffect(() => {
-    if (liff.isLoggedIn()) {
-      fetchUserProfile();
+    const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
+
+    if (!liffId) {
+      console.error('LIFF ID is missing');
+      return;
     }
+
+    liff
+      .init({ liffId })
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          fetchUserProfile();
+        } else {
+          liff.login();
+        }
+      })
+      .catch((error) => {
+        console.log('LIFF initialization failed', error);
+      });
   }, []);
 
   const fetchUserProfile = async () => {
@@ -94,3 +49,6 @@ export default function Header() {
     </header>
   );
 }
+
+
+
