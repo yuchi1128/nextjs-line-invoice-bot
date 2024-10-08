49dd070dd36f6972a76b7a1b150aee842d4690b8
yuchi1128 <yukiya20021128@icloud.com>
2024-10-08 20:17:21 +0900

    履歴ページ

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,13 +1,13 @@
 //src/app/record/page.tsx
 import React from 'react'
-import InvoiceHistory from '@/app/components/InvoiceHistory';
+// import InvoiceHistory from '@/app/components/InvoiceHistory';
 // import Navigation from '@/app/components/Navigation';
 
 const Record = () => {
   return (
     <div>
         <div>Record</div>
-        <InvoiceHistory />
+        {/* <InvoiceHistory /> */}
         {/* <Navigation /> */}
     </div>
   )
