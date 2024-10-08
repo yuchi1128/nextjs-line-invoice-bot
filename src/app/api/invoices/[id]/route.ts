e61fca25c0d74d7bdeaff5bbf7c7d789bd84262f
yuchi1128 <yukiya20021128@icloud.com>
2024-10-08 20:39:19 +0900

    履歴ページ

--- src/app/api/invoices/[id]/route.ts
+++ src/app/api/invoices/[id]/route.ts
@@ -16,5 +16,5 @@ export async function PATCH(request: Request, { params }: { params: { id: string
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
   const id = parseInt(params.id);
   await prisma.invoice.delete({ where: { id } });
-  return NextResponse.json({ message: '請求書が正常に削除されました' });
+  return NextResponse.json({ message: '請求書が正常に削除されました。' });
 }
