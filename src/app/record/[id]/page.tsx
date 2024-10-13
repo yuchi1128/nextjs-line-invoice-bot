7c1e9ca74a8f2ac50fe03248726f069dee9c09fa
yuchi1128 <yukiya20021128@icloud.com>
2024-10-14 03:02:24 +0900

    デザイン修正

--- src/app/record/[id]/page.tsx
+++ src/app/record/[id]/page.tsx
@@ -103,7 +103,7 @@ const InvoiceDetail = () => {
             <button
               onClick={togglePaidStatus}
               className={`px-6 py-5 rounded-full text-xl font-bold transition duration-300 ease-in-out w-48 ${
-                invoice.isPaid ? 'bg-green-100 text-green-800 hover:bg-green-600' : 'bg-red-100 text-red-800 hover:bg-red-600'
+                invoice.isPaid ? 'bg-green-200 text-green-800 hover:bg-green-400' : 'bg-red-200 text-red-800 hover:bg-red-400'
               }`}
             >
               {invoice.isPaid ? '精算済み' : '未精算'}
