a9a4b9f1b98fd33141b98d9a644d32b0e4a8c12e
yuchi1128 <yukiya20021128@icloud.com>
2024-12-14 13:01:13 +0900

    liffオブジェクトをstate管理

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import './globals.css';
+import { LiffProvider } from './context/LiffProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <LiffProvider>
+          {children}
+        </LiffProvider>
+      </body>
     </html>
   );
 }
