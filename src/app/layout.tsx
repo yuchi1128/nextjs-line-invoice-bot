baa7456608b177718abef526cf4201b1c169440b
yuchi1128 <yukiya20021128@icloud.com>
2024-12-28 23:22:17 +0900

    ヘッダーの呼び出し位置を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import { LiffProvider } from '@/context/LiffProvider';
-import Header from '@/components/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
